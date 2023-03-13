1b95f8be49934f9919f1686ea3f1acfe8a4bc59a
t-khlpk <110490094+t-khlpk@users.noreply.github.com>
2023-03-13 20:58:07 +0200

    hm7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const cardsMarkup = createGallery(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+ function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+      `<div class="gallery__item">
+            <a class = "gallery__link"
+                href= "${original}" 
+                onclick = "return false">
+            <img  class="gallery__image"
+                src="${preview}" 
+                alt = "${description}" 
+                data-source = "${original}"
+            >
+            </a>
+        </div>`)
+    .join("");
+};
+
+gallery.addEventListener(`click`, onClickGallery);
+
+function onClickGallery(evt) {
+    // console.log(evt.target);
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}"
+        width="1000" height="800"
+        onclick="return false">
+        `,
+        {
+        onShow: (instance) => {
+          document.addEventListener("keydown", (evt) => {
+            if (evt.code === "Escape") {
+              instance.close();
+            }
+        
+          })
+          },
+        onClose: (instance) => {
+          document.removeEventListener("keydown", evt)
+        },
+      })
+        .show(); 
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const cardsMarkup = createGallery(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+    `<li>
+        <a class="gallery__item"
+            href="${original}"
+            onclick = "return false">
+        <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}"/>
+        </a>
+        </li>`)
+    .join("");
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    overlay : true,
+    overlayOpacity : 0.8,
+    captionsData : `alt`,
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
